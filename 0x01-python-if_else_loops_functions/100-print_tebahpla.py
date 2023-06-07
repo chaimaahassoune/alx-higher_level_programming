3b6fadd6034a26c06a2d59265e76df4e9bd7790e
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-06-06 19:34:02 -0700

    nice task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(ord('z'), ord('A') - 1, -1):
-    if letter % 2 == 0:
-        print("{}".format(chr(letter)), end="")
-    else:
-        print("{}".format(chr(letter).upper()), end="")
+
+toggle = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - toggle)), end="")
+    toggle = 32 if toggle == 0 else 0
