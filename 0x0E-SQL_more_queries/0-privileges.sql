13ec22dcae1545e5b61e657cc03c7ec6bad889ef
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-08-16 18:49:20 -0700

    SQL

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2;
-echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
